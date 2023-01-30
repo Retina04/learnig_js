93a0ec67cc2558ba4c4e3b06de751c944afb1ded
Retina04 <retinaislam2019nida@gmail.com>
2023-01-31 00:28:49 +0600

    pk

---
+++ feet_to_inch.js
@@ -0,0 +1,14 @@
+// function inchToFeet(inch) {
+//   Feet = inch / 12;
+// }
+// console.log("Feet :", );
+// const dadaInches = 144;
+// const dadaFeet = dadaInches / 12;
+// console.log("dadaFeet ", dadaFeet);
+
+function inchToFeet(inches) {
+  const feet = inches / 12;
+  return feet;
+}
+const dadaInches = 144;
+const dadaFeet = inchToFeet(dadaInches);
